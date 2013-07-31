21c2cef30d4c84f2e8ecad83a107275b8d7718f3
Steve Halford <steve@inknpixel.co.uk>
2013-07-31 19:36:29 +0100

    Add a Job model and a route to return all Jobs

---
+++ app/models/Job.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model
+{
+    protected $table = 'jobs';
+
+}
--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,9 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('/jobs', function() {
+    $jobs = Job::all();
+
+    return Response::json($jobs->toArray(), 200);
+});
