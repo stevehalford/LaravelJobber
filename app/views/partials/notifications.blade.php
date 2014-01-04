7387b70e91666b99bf8f294d61e2070df40b1d10
Steve Halford <steve@inknpixel.co.uk>
2014-01-04 16:30:45 +0000

    Styling for notifications

--- app/views/partials/notifications.blade.php
+++ app/views/partials/notifications.blade.php
@@ -1,34 +1,29 @@
 @if ($errors->any())
 <div class="alert alert-error alert-block">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
     Please check the form below for errors
 </div>
 @endif
 
 @if ($message = Session::get('success'))
 <div class="alert alert-success alert-block">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
     {{ $message }}
 </div>
 @endif
 
 @if ($message = Session::get('error'))
 <div class="alert alert-error alert-block">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
     {{ $message }}
 </div>
 @endif
 
 @if ($message = Session::get('warning'))
 <div class="alert alert-warning alert-block">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
     {{ $message }}
 </div>
 @endif
 
 @if ($message = Session::get('info'))
 <div class="alert alert-info alert-block">
-    <button type="button" class="close" data-dismiss="alert">&times;</button>
     {{ $message }}
 </div>
 @endif
