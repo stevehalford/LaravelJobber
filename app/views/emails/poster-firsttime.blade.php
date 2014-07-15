36d39ed6d72aa2848372dab4c32c5f9561d80295
Steve Halford <steve@bipsync.com>
2014-07-15 22:50:59 +0100

    Trying to fix emails again

--- app/views/emails/poster-firsttime.blade.php
+++ app/views/emails/poster-firsttime.blade.php
@@ -1,11 +1,11 @@
-Hello! :)
-\r\n
-We apologize for the inconvenience, but since this is the first time you post with this e-mail address, we need to manually verify it.\r\n
-Thank you for your patience, as the ad should be published ASAP. We'll send you an e-mail when that happens!
-\r\n
+Hello! :) 
+ 
+We apologize for the inconvenience, but since this is the first time you post with this e-mail address, we need to manually verify it. 
+Thank you for your patience, as the ad should be published ASAP. We'll send you an e-mail when that happens! 
+ 
 From now on, every ad you post with this e-mail address will instantly be published.
-\r\n
----
-\r\n
-Thank you for using our service!
-Steve
+ 
+--- 
+ 
+Thank you for using our service! 
+Steve 
