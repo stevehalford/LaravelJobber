28a67fdff2f2739f4121c9e90f4ecd4218c223f7
Steve Halford <steve@bipsync.com>
2014-07-15 22:35:29 +0100

    Comment out influads

--- app/views/partials/influads.blade.php
+++ app/views/partials/influads.blade.php
@@ -1,2 +1,3 @@
-<div id="influads_block" class="sidebar_block influads_block"> </div>
+<!-- <div id="influads_block" class="sidebar_block influads_block"> </div>
 <script type="text/javascript">(function(){ var acc =   "acc_243d287_pub";var st ="css";var or= "v";var e=document.getElementsByTagName("script")[0];var d=document.createElement("script");d.src=('https:' == document.location.protocol ?'https://' : 'http://') +"engine.influads.com/show/"+or+"/"+st+"/"+acc;d.type="text/javascript"; d.async=true; d.defer=true; e.parentNode.insertBefore(d,e);})();</script>
+ -->
