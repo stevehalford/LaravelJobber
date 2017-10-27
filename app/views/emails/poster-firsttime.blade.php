b3e0f75dd75346d2791e637e4bbbcce2114deac7
Steve Halford <steve@bipsync.com>
2017-10-27 14:37:23 +0100

    Some grammar fixes for emails

--- app/views/emails/poster-firsttime.blade.php
+++ app/views/emails/poster-firsttime.blade.php
@@ -1,11 +1,11 @@
-Hello! :) 
- 
-We apologize for the inconvenience, but since this is the first time you post with this e-mail address, we need to manually verify it. 
-Thank you for your patience, as the ad should be published ASAP. We'll send you an e-mail when that happens! 
- 
+Hello! :)
+
+We apologize for the inconvenience, but since this is the first time you have posted with this e-mail address, we need to manually verify it.
+Thank you for your patience, the ad should be published ASAP. We'll send you an e-mail when that happens! 
+
 From now on, every ad you post with this e-mail address will instantly be published.
- 
---- 
- 
-Thank you for using our service! 
-Steve 
+
+---
+
+Thank you for using our service!
+Steve
--- app/views/emails/poster-publish.blade.php
+++ app/views/emails/poster-publish.blade.php
@@ -1,12 +1,14 @@
-Hello! :) 
- 
-Your ad was published and is available at: {{ URL::action('JobController@show', array($job->id, $job->getSlug())) }} 
- 
---- 
-Edit: {{ URL::action('JobController@edit', array($job->id, $job->auth)) }} 
-Deactivate: {{ URL::action('JobController@deactivate', array($job->id, $job->auth)) }} 
- 
---- 
- 
-Thank you for using our service! 
+Hello! :)
+
+Your ad was published and is available at: {{ URL::action('JobController@show', array($job->id, $job->getSlug())) }}
+
+Please keep this email handy as the links below will allow you to edit or deactivate the ad should you wish to in the future.
+
+---
+Edit: {{ URL::action('JobController@edit', array($job->id, $job->auth)) }}
+Deactivate: {{ URL::action('JobController@deactivate', array($job->id, $job->auth)) }}
+
+---
+
+Thank you for using our service!
 Steve
